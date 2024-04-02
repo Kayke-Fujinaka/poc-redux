99c4dc80a9915a0c2fa0b86a1ab0709516a118cc
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2024-04-02 10:07:29 -0300

    feat: add counter in application

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
-import "./App.css";
+import { useDispatch, useSelector } from "react-redux";
+
+import { IReduxState } from "./interfaces/store";
+import { discrement, increment, reset } from "./store/ducks/counter";
 
 function App() {
+  const dispatch = useDispatch();
+  const counterValue = useSelector((state: IReduxState) => state.counter.value);
+
   return (
     <div>
-      <h1>React APP</h1>
+      <h1>Contador</h1>
+      <p>Valor: {counterValue}</p>
+      <button onClick={() => dispatch(increment())}>Incrementar</button>
+      <button onClick={() => dispatch(discrement())}>Decrementar</button>
+      <button onClick={() => dispatch(reset())}>Resetar</button>
     </div>
   );
 }
--- src/interfaces/store.d.ts
+++ src/interfaces/store.d.ts
@@ -1,2 +1,8 @@
+import { ICounter } from "./counter";
+
 export type AppDispatch = typeof store.dispatch;
 export type AppThunk = ThunkAction<void, RootState, unknown, Action<string>>;
+
+export type IReduxState = {
+    counter: ICounter
+}
